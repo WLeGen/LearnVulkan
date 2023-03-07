0c0867d65136c68014ca9b440a5a4781e4344575
W-LG <w18163718108@163.com>
2023-03-07 21:54:14 +0800

    init the cmake project 'hello world'

---
+++ LearnVulkan/src/LearnVulkan.cpp
@@ -0,0 +1,14 @@
+﻿// LearnVulkan.cpp: 定义应用程序的入口点。
+//
+
+#include "LearnVulkan.h"
+#include "LearnVulkanConfig.h"
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	std::cout << "Version " << LearnVulkan_VERSION_MAJOR << "."
+		<< LearnVulkan_VERSION_MINOR << std::endl;
+	std::cin.get();
+	return 0;
+}
